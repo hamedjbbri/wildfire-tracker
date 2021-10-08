42866cd2cf64e02f086cccdb4beeba6bdc09e90d
Hamed Jabbari <jabbary.hamed@gmail.com>
2021-10-08 11:12:58 -0700

    Google map loaded

---
+++ src/components/Map.js
@@ -0,0 +1,28 @@
+import GoogleMapReact from 'google-map-react'
+
+const Map = ({ center, zoom }) => {
+    return (
+        <div className='map'>
+            <GoogleMapReact
+                bootstrapURLKeys={{
+                    key: 'AIzaSyC49ILwXgUmdFN4_RDyN_XoZ50XJXlWBA0'
+                }}
+                defaultCenter = { center }
+                defaultZoom = { zoom }
+            >
+                
+            </GoogleMapReact>
+        </div>
+    )
+}
+
+Map.defaultProps = {
+    center: {
+        lat: 42.3265,
+        lng: -122.8756
+    },
+
+    zoom: 6
+}
+
+export default Map;
