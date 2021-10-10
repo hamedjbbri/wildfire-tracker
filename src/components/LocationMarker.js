5fbd111051ac925a08fbd2d19e73089aa7eeb078
Hamed Jabbari <jabbary.hamed@gmail.com>
2021-10-10 08:07:27 -0700

    Adding wildfires icon for all the map

--- src/components/LocationMarker.js
+++ src/components/LocationMarker.js
@@ -4,9 +4,9 @@ import locationIcon from '@iconify/icons-mdi/fire-alert';
 const LocationMarker = ({ lat, lng, onClick }) => {
     return (
         <div className='location-marker' onClick={onClick}>
-            <Icon icon={locationIcon} className={location-icon} /> 
+            <Icon icon={locationIcon} className="location-icon" /> 
         </div>
     )
 }
 
-export default LocationMarker
+export default LocationMarker;
